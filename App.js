c05e030eb7cdabd3cebe638eb5137e3ba8a3df28
Atoon <Atoonchanmalakeoxaisy@gmail.com>
2024-02-25 07:15:07 +0700

    add file App.js

---
+++ App.js
@@ -0,0 +1,4 @@
+let enterInput = document.getElementById("enter");
+let input = document.getElementById("username");
+let ul = document.querySelector("ul");
+let item = document.getElementsByTagName("li");
