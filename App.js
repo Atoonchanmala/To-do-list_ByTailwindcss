7359c891dd8bf892f3408cea11316cdd5db9f977
Atoon <Atoonchanmalakeoxaisy@gmail.com>
2024-02-27 08:36:27 +0700

    Success Javascript in file App.js

--- App.js
+++ App.js
@@ -1,52 +1,54 @@
 let enterInput = document.getElementById("enter");
-let input = document.getElementById("username");
-let ul = document.querySelector("ul");
+let input = document.getElementById("userInput");
+let ul = document.getElementById("myList");
 let item = document.getElementsByTagName("li");
 
 const inputLength = () => {
-    return input.value.length;
+    return input.value.trim().length;
 }
+
 const listLength = () => {
     return item.length;
 }
 
 // Function create Element
-const creteElement = () => {
+const createElement = () => {
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
+    li.className = "font-semibold w -full px-2 py-2 bg-blue-600 text-white text-sm rounded-lg border-3 border-rose-400  hover:bg-blue-700";
     ul.appendChild(li);
     input.value = "";
 
     // To do list style success
-    const ElementSuccess = () => {
+    const elementSuccess = () => {
         li.classList.toggle("done");
     }
-    li.addEventListener("click", ElementSuccess);
+    li.addEventListener("click", elementSuccess);
 
     // To do list Delete
-    const ElementDelete = () => {
-        li.classList.add("delete");
+    const elementDelete = () => {
+        li.parentNode.removeChild(li);
     }
 
     // Crete delete item list
     let dBtn = document.createElement("button");
-    dBtn.appendChild(document.createTextNode("i"));
-    li.appendChild(dBtn);
+    dBtn.className = "font-bold text-rose-500 text-lg ml-3 text-end";
+    dBtn.innerHTML = '<i class="fa-solid fa-paw"></i>';
 
-    dBtn.addEventListener("click", ElementDelete);
+    dBtn.addEventListener("click", elementDelete);
+
+    li.appendChild(dBtn);
 }
 // Function After click element
-const ItemAfteClick = () => {
+const itemAfterClick = () => {
     if (inputLength() > 0) {
-        creteElement();
+        createElement();
     }
 }
-const ItemAfterClickKeypress = (event) => {
+const itemAfterClickKeypress = (event) => {
     if (inputLength() > 0 && event.which === 13) {
-        creteElement();
+        createElement();
     }
 }
-enterInput.addEventListener("click", ItemAfteClick);
-input.addEventListener("keypress", ItemAfterClickKeypress);
-
-                // kae error mai
+enterInput.addEventListener("click", itemAfterClick);
+input.addEventListener("keypress", itemAfterClickKeypress);
